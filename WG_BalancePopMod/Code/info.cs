872533090e7e3d7ae408b03a12b087e64dcf3eb4
WhitefangGreytail <whitefang_greytail@yahoo.com.au>
2016-02-07 21:46:17 +0800

    v6.1a - Mesh names are now one per line

--- WG_BalancePopMod/Code/info.cs
+++ WG_BalancePopMod/Code/info.cs
@@ -12,7 +12,7 @@ namespace WG_BalancedPopMod
     {
         public string Name
         {
-            get { return "WG Realistic Population v6.0"; }
+            get { return "WG Realistic Population v6.1"; }
         }
         public string Description
         {
